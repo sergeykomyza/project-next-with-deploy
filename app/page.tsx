67299d372bce2cc88cc62a9657925df4f8a8d6ee
Сергей Комыза <s.komiza@yandex.ru>
2026-01-12 20:53:47 +0300

    add cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
-import { PrismaClient, Product } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
+import type { Product } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
