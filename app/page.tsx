93560c0c1a664645db4611c4fdaa5047f5b3fe30
Сергей Комыза <s.komiza@yandex.ru>
2026-01-13 21:16:45 +0300

    styled cards

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,18 @@ export default async function Home() {
         <div className="catalog">
           {
             products.map((product) => (
-              <div key={product.id}>
+              <div key={product.id} className="product">
                 <Image
-                  className="" 
+                  className="productFoto" 
                   src={product.foto || '/no_image.gif'} 
                   alt='main foto' 
                   width={300} 
                   height={300} 
                 />
-                <h2>{product.name}</h2>
-                <p>{product.description}</p>
-                <p>${product.price}</p>
+                {/* <p className="productDescription">{product.description}</p> */}
+                <p className="productDescription">On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will</p>
+                <h2 className="productName">{product.name}</h2>
+                <p className="productPrice">${product.price}</p>
               </div>
             ))
           }
